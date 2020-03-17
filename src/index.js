3f5051a97a5413fdd93194490596d29fc51e689a
Kate Fd <katesfadeeva@gmail.com>
2020-03-17 18:26:18 +0300

    init: start task Morse code decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,41 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const MORSE_BOOLEAN = {
+    "10": ".",
+    "11": "-"
+  }
+
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+  for (let i = 0, charsLength = expr.length; i < charsLength; i += 10) {
+    arr.push(expr.substring(i, i + 10));
+  }
+  for (let k = 0; k < arr.length; k++) {
+    let simbolArray = arr[k].split('');
+    let newarr = [];
+
+    for (let j = simbolArray.length-1; j>=0; j--) {
+       newarr.unshift(simbolArray[j-1]+simbolArray[j]);
+       j = j-1;
+    }
+    
+    for (let t = 0; t < newarr.length; t++) {
+      if (newarr[t] in MORSE_BOOLEAN) {
+        newarr[t] = MORSE_BOOLEAN[newarr[t]];
+      } else {
+        newarr[t] = "";      
+      }
+    }
+    
+    let str = newarr.join('');
+    if (str in MORSE_TABLE) {
+      str = MORSE_TABLE[str];
+      arr[k] = str;
+    } else {
+      arr[k] = " ";
+    }   
+  } return(arr.join(''));
 }
 
 module.exports = {
